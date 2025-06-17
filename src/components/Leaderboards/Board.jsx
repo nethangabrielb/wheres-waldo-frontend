7ac0283aa499c25d68bec8113cd42b6b15e76cac
suix0 <112095627+suix0@users.noreply.github.com>
2025-06-17 13:04:54 +0800

    style: custom scrollbar for leaderboard

--- src/components/Leaderboards/Board.jsx
+++ src/components/Leaderboards/Board.jsx
@@ -1,6 +1,12 @@
 const Board = ({ game }) => {
   return (
-    <div className="flex flex-col items-center text-lg border  p-4 h-[400px] w-full overflow-auto bg-secondary text-last shadow-board hover:shadow-board-hover hover:-translate-2 hover:-translate-y-2 transition-all">
+    <div
+      className="flex flex-col items-center text-lg border  p-4 h-[400px] w-full overflow-auto bg-secondary text-last shadow-board hover:shadow-board-hover hover:-translate-2 hover:-translate-y-2 transition-all [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]
+  [&::-webkit-scrollbar-track]:bg-last
+  [&::-webkit-scrollbar-thumb]
+  [&::-webkit-scrollbar-thumb]:bg-main"
+    >
       <h1 className="text-2xl font-bold">{game.name}</h1>
       <div className="flex justify-between w-full">
         <p className="font-bold">Name</p>
